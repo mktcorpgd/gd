a569db11d79a7680d6b73d7d5a654049a732b35b
Nicolás <nicolas.alonso@datco.net>
2022-02-18 10:44:14 -0300

    scr

--- somos/custom-front/custom-front-altas.js
+++ somos/custom-front/custom-front-altas.js
@@ -9,21 +9,20 @@ jQuery(document).ready(function() {
 		}
 		else {
 			jQuery('select[name^=ACCESS] option[value="Usuario de red"],select[name^=ACCESS] option[value="Email"]').prop('selected', true)
-			jQuery('select[name^=ACCESS] option[value="Screentime"]').prop('selected', false)
+			jQuery('select[name^=ACCESSinput[name=OPC]'] option[value="Screentime"]').prop('selected', false)
 		}
 	});
 
 
 	// Si se marca checkbox: agregar valor a [OPC]
 	jQuery('#check_freelance').change(function() {
-		var opc_checked = jQuery('input[name=OPC]:checked').val();
-		console.log(opc_checked);
 		if ( this.checked ) {
-			jQuery('input[name=OPC]').val(opc_checked+' - '+jQuery(this).val());
+			jQuery('input[name=SCRCHECK]').val(' + '+this.value);
 		}
 		else {
-			jQuery('input[name=OPC]').val(opc_checked);
+			jQuery('input[name=SCRCHECK]').empty();
 		}
+		console.log(jQuery('input[name=SCRCHECK]').val());
 	});
 
 	
