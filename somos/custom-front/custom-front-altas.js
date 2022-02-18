023e010627a4edd776e839c4cbf68fb7603000b2
Nicolás <nicolas.alonso@datco.net>
2022-02-18 11:43:10 -0300

    checked

--- somos/custom-front/custom-front-altas.js
+++ somos/custom-front/custom-front-altas.js
@@ -17,11 +17,8 @@ jQuery(document).ready(function() {
 	// Si se marca checkbox: agregar valor a [OPC]
 	jQuery('#check_freelance input').change(function() {
 		if ( this.checked ) {
-			var radio_checked = jQuery('input[name=OPC]:checked').val();
 			var checkbox_checked = jQuery(this).val();
-			console.log(radio_checked);
-			console.log(checkbox_checked);
-			jQuery('input[name=OPC]:checked').val(radio_checked+' + '+checkbox_checked);
+			jQuery('input[name=SRCCHECK]').val(' + '+checkbox_checked);
 		}
 	});
 
