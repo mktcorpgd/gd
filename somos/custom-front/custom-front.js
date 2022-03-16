e149056e4586d86761aec05196b058a9189b4f59
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2022-03-16 14:39:19 -0300

    somos

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -66,7 +66,7 @@ jQuery(document).ready(function() {
 			var new_title = jQuery(this).text();
 			new_title = new_title.substring(new_title.indexOf('.')+2,new_title.length);
 			jQuery(this).text(new_title);
-			jQuery(this).replace(/(?:\r\n|\r|\n)/g, '<br>');
+			jQuery(this).text().replace(/(?:\r\n|\r|\n)/g, '<br>');
 		});
 	}
 	
