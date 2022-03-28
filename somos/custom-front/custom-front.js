ad8f0ed1e02f94b15c051930d73048ac4171d1b3
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2022-03-28 17:28:33 -0300

    url

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -473,7 +473,8 @@ jQuery(document).ready(function() {
 
 
 	// FORMULARIOS - Ir a página en cambio de select para .nav-posts
-	jQuery('.nav-posts select').val(window.location);
+	console.log(window.location.href);
+	jQuery('.nav-posts select').val(window.location.href);
 	jQuery(document).on('change','.nav-posts select', function() {
 			var url = jQuery(this).val();
 		if (url) {
