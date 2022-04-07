4f1ac3177d5c217547467d80d084d2b1236bbb49
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2022-04-07 17:06:21 -0300

    altas

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -390,11 +390,11 @@ jQuery(document).ready(function() {
 		if ( jQuery(this).text().indexOf('—') > -1 ) {
 			var input_value = jQuery(this).text();
 			if ( input_value != '—' ) {
-				jQuery(this).attr('disabled','disabled');
 				if ( jQuery(this).parent().attr('name') == 'CC_UNIT' ) {
 					input_value = input_value.substring(input_value.indexOf('—')+2,input_value.length);
 				}
 				else {
+					jQuery(this).attr('disabled','disabled');
 					input_value = input_value.substring(input_value.indexOf('—'),input_value.length);
 				}
 				jQuery(this).text(input_value).val(input_value);
