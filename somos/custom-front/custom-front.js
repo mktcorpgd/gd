342b3081e5996633f2e4bb1293c4ff0afd0a1258
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2022-04-21 11:52:13 -0300

    sel_index

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -496,9 +496,12 @@ jQuery(document).ready(function() {
 
 	// FORMULARIOS - Mostrar aviso si eligen opciones: "bolsa", "lapicera" o "cuaderno"
 	jQuery('select[name^=MERCH]').on('change',function(e) {
+		var sel_index = jQuery(this).attr('name');
+		sel_index = sel_index.substring(sel_index.indexOf('MERCH')+5,sel_index.length);
+		console.log('sel_index='+sel_index);
 		var opt_index = jQuery(this).prop('selectedIndex');
 		if ( opt_index == 1 || opt_index == 2 || opt_index == 3 || opt_index == 4 ) {
-			console.log(jQuery(this).find('.wpcf7-response-info').length);
+			console.log(jQuery('select[name=MERCH'+sel_index+']').find('.wpcf7-response-info').length);
 			/*if ( jQuery(this).find('.wpcf7-response-info').length == -1 ) {
 			}*/
 			jQuery('<div class="wpcf7-response-info">Sin costo hasta 10 unidades</div>').insertAfter(this);
