ab4bb01547fe621d0779379a79af2d0405cbbc17
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-04-22 17:59:16 -0300

    referer

--- www/js/custom.js
+++ www/js/custom.js
@@ -426,7 +426,7 @@ jQuery(document).ready(function() {
 		});
 		if ( jQuery('input[name*="SRC"]:not(.manual)').length ) {
 			if ( jQuery('input[name*="SRC"]').val().length == 0 ) {
-				jQuery('input[name*="SRC"]').val(window.location.href);
+				jQuery('input[name*="SRC"]').val(jQuery('body').data('referer').'=>'.window.location.href);
 			}
 			else if ( jQuery('input[name="HREF"]').length ) {
 				jQuery('input[name="HREF"]').val(window.location.href);
