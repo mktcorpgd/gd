b5303feaf69d40dc8dbaf253d1bda07dbac02b9c
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-06-01 17:28:03 -0300

    obj_scosto

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -491,6 +491,7 @@ jQuery(document).ready(function() {
 			return false;
 		}
 	});
+	console.log('obj_scosto='+obj_scosto);
 	var limit_obj_scosto = obj_scosto*2-2;
 	console.log('limit_obj_scosto='+limit_obj_scosto);
 	jQuery('input[name^=CANT]').keyup(function(e) {
