cab42ec9f9a828878970f4f67788a6991a86b16f
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2022-06-21 16:16:14 -0300

    place

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -544,6 +544,7 @@ jQuery(document).ready(function() {
 		autocomplete.addListener("place_changed", function() {
 			selected = true;
 			var place = autocomplete.getPlace();
+			console.log(place);
 			jQuery(this).after('<img src=https://maps.googleapis.com/maps/api/staticmap?center='+place+'&zoom=16&size=400x400&key=AIzaSyBgKiaX5D3Pp4Jx16S_JBH4_vUJngmX3PM" width="200" height="200" />');
 		});
 	}
