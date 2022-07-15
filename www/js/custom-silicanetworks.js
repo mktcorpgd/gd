3664de092bc57e351f5725453b27961abb98f743
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-07-15 11:53:26 -0300

    fusion-portfolio.destacados

--- www/js/custom-silicanetworks.js
+++ www/js/custom-silicanetworks.js
@@ -5,4 +5,17 @@ jQuery(document).ready(function() {
 		jQuery('#wpcf7-f31529-p32849-o1 .wpcf7-submit').val('Descargar mapa de red');
 	}
 
+	// Animaciones en destacados
+	jQuery('.fusion-portfolio.destacados .fusion-portfolio-content-wrapper').hover(
+		function() {
+			var gif_img = jQuery(this).find('.wp-post-image').attr('src');
+			gif_img = gif_img.substring(gif_img.indexOf('s-icon-web-')+11,gif_img.indexOf('-frame'));
+			jQuery(this).find('.wp-post-image').attr('srcset','/wp-content/uploads/sites/5/s-icon-web-'+gif_img+'.gif');
+		}, function() {
+			var gif_img = jQuery(this).find('.wp-post-image').attr('src');
+			gif_img = gif_img.substring(gif_img.indexOf('s-icon-web-')+11,gif_img.indexOf('-frame'));
+			jQuery(this).find('.wp-post-image').attr('srcset','/wp-content/uploads/sites/5/s-icon-web-'+gif_img+'-frame.jpg');
+		}
+	);
+
 });
