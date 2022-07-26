c08fae2120b6a780a6bc75d2a3248f2b7071ea07
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2022-07-26 15:56:57 -0300

    jQuery('#mobile-nav .menu li').each

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -106,9 +106,13 @@ jQuery(document).ready(function() {
 		headerMobile();
 	});
 	jQuery('#mobile-nav-button').click(function(e) {
-		console.log('mobile-nav-button');
 		jQuery(this).toggleClass('gp-active');
 		jQuery('#mobile-nav').toggle();
+	});
+	jQuery('#mobile-nav .menu li').each(function() {
+		if ( jQuery(this).find('ul').length > 0 ) {
+			jQuery('<i class="mobile-dropdown-icon" />').insertAfter(jQuery(this).children(':first'));		
+		}		
 	});
 
 
