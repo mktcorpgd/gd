7e7f04034edc4679d54fd6ee654e07f83e36de37
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-08-11 11:09:59 -0300

    inputtext_name

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -403,7 +403,6 @@ jQuery(document).ready(function() {
 				if ( checkbox_name == 'ALOJNOT[]' || checkbox_name == 'AUTONOT[]' ) {
 					inputtext_name = inputtext_name.substr(0,5);
 					jQuery('input[name^='+inputtext_name+']').val('').removeAttr('readonly').removeAttr('disabled');
-					jQuery('input[name^='+inputtext_name+']').val('');
 				}
 				else {
 					jQuery('input[name='+inputtext_name+']').val('').removeAttr('readonly').removeAttr('disabled');
