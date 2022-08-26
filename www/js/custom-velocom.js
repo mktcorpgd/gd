6880cf0921f7d457dfeac57be683030b63b930c2
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-08-26 15:00:50 -0300

    jQuery('select[name="LOC

--- www/js/custom-velocom.js
+++ www/js/custom-velocom.js
@@ -2,7 +2,7 @@
 var normalize=(function(){var from="ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",to="AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",mapping={};for(var i=0,j=from.length;i<j;i++)mapping[from.charAt(i)]=to.charAt(i);return function(str){var ret=[];for(var i=0,j=str.length;i<j;i++){var c=str.charAt(i);if(mapping.hasOwnProperty(str.charAt(i))){ret.push( mapping[c]);}else{ret.push(c);}}return ret.join('').replace(/[^-A-Za-z0-9]+/g,'-').toLowerCase();}})();
 
 
-// HEADER - Transformar teléfonos con link:
+// HEADER - Transformar teléfonos en link:
 jQuery('.fusion-contact-info-phone-number').each(function(i) {
 	var value = jQuery(this).text();
 	jQuery(this).html('<a href="tel:'+value+'">'+value+'</a>');
@@ -13,26 +13,33 @@ jQuery('.fusion-contact-info-phone-number').each(function(i) {
 jQuery.getPrm = function(name){var results=new RegExp('[?&]'+name+'=([^&#]*)').exec(window.location.href);if(results==null){return null;}else{return results[1]||0;}}
 
 
-// CONTENIDO - Cambios para páginas de Internet: mostrar/ocultar según variable GET <lugar>
-if ( jQuery('body').hasClass('postid-24725') || jQuery('body').hasClass('postid-23640') ) {
-	var lugar_name = decodeURIComponent(jQuery.getPrm('lugar'));
-	var lugar_class = normalize(lugar_name.toLowerCase());if(lugar_class.slice(-1)=='-'){lugar_class=lugar_class.slice(0,-1);}
-	if ( lugar_class == 'null' && window.location.href.indexOf('internet') > 0 ) {
+// CONTENIDO - Página dinámica para servicio de internet
+var lugar_name = decodeURIComponent(jQuery.getPrm('lugar'));
+var lugar_class = normalize(lugar_name.toLowerCase());if(lugar_class.slice(-1)=='-'){lugar_class=lugar_class.slice(0,-1);}
+var site_name = jQuery('meta[property=og:site_name]').attr('content');
+console.log(site_name);
+if ( jQuery('body').hasClass('home') || jQuery('body').hasClass('single-avada_portfolio') ) {
+	if ( lugar_class == 'null' ) {
 		jQuery('.caption,.price span,#legales').hide();
 	}
 	else {
-		jQuery('.map iframe').attr('src','https://www.velocom.com.ar/_velocom/cobertura/'+lugar_class);
-		jQuery('.map').show();
-		if ( window.location.href.indexOf('corporativo') > 0 ) {
-			jQuery('title').html('Internet Corporativo en '+lugar_name+' &mdash; Velocom');
+		if ( jQuery('.map iframe').length ) {
+			jQuery('.map iframe').attr('src','https://www.velocom.com.ar/_velocom/cobertura/'+lugar_class);
+			jQuery('.map').show();	
 		}
-		else {
-			jQuery('title').html('Internet para Hogares en '+lugar_name+' &mdash; Velocom');
+		if ( jQuery('body').hasClass('postid-24725') ) {
+			jQuery('title').html('Internet para Hogares en '+lugar_name+' &mdash; '+site_name);
+		}
+		else if ( jQuery('body').hasClass('postid-23640') ) {
+			jQuery('title').html('Internet Corporativo en '+lugar_name+' &mdash; '+site_name);
+		}
+		else if ( jQuery('body').hasClass('postid-25544') ) {
+			jQuery('title').html('Internet por Fibra Óptica en '+lugar_name+' &mdash; '+site_name);
 		}
 		jQuery('.not-selected').removeClass('not-selected');
 		jQuery('div:not(.not-selected) .'+lugar_class+'+.not').hide();
 		jQuery('select[name="LOC"] option.'+lugar_class+',select[name="LOC"] option:contains('+lugar_name+')').attr('selected','selected');
-		jQuery('div:not(.not-selected) .'+lugar_class+',.tfs-slider h2 span.'+lugar_class+',.caption span.'+lugar_class+',#legales .panel-body p span.'+lugar_class+',#legales .panel-body p .cur_month,#legales .panel-body p .cur_year,#legales .panel-body p .last_day,.fusion-title.step2,fusion-separator.step2').show();
+		jQuery('div:not(.not-selected) .'+lugar_class+',.tfs-slider h2 span.'+lugar_class+',.caption span.'+lugar_class+',#legales .panel-body p span.'+lugar_class+',#legales .panel-body p .cur_month,#legales .panel-body p .cur_year,#legales .panel-body p .last_day,.step2').show();
 		if ( lugar_class == 'bariloche' ) {
 			jQuery('.hidden-bariloche').hide();
 		}
@@ -42,24 +49,12 @@ if ( jQuery('body').hasClass('postid-24725') || jQuery('body').hasClass('postid-
 
 // CONTENIDO - Si cambia la localidad recargar con información respectiva
 jQuery('select[name*=LOC]').change(function() {
-	if ( jQuery('body').hasClass('home') || jQuery('body').hasClass('postid-24725') || jQuery('body').hasClass('postid-23640') ) {
-		if ( !jQuery('body').hasClass('modal-open') && jQuery(this).val() != '' ) {
+	var lugar_name = jQuery(this).val();
+	if ( jQuery('body').hasClass('home') || jQuery('body').hasClass('single-avada_portfolio') ) {
+		if ( !jQuery('body').hasClass('modal-open') || lugar_name != '' ) {
 			jQuery('#cargando-btn').trigger('click');
-			var lugar_name = jQuery(this).val();
-			var lugar_class = normalize(lugar_name);
-			if(lugar_class.slice(-1)=='-'){lugar_class=lugar_class.slice(0,-1);}
 			setTimeout(function(){
-				if ( jQuery('body').hasClass('home') ) {
-					document.location = 'https://www.velocom.com.ar/internet/?lugar='+lugar_name;
-				}
-				else if ( window.location.href.indexOf('?') > -1 ) {
-					var url_parts = window.location.href.split('?');
-					var main_url = url_parts[0];
-					document.location = main_url+'?lugar='+lugar_name;
-				}
-				else {
-					document.location = window.location.href+'?lugar='+lugar_name;
-				}
+				document.location = window.location.href+'?lugar='+lugar_name;
 			},500);
 		}
 	}
