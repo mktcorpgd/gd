045c3e967dc189243f8a5cb9d1d49c21d31a33b0
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-08-26 17:12:38 -0300

    lugar_index

--- www/js/custom-velocom.js
+++ www/js/custom-velocom.js
@@ -56,7 +56,7 @@ jQuery('select[name*=LOC]').change(function() {
 	var lugar_index = jQuery('option:selected',this).index();
 	console.log(lugar_index);
 	if ( jQuery('body').hasClass('home') || jQuery('body').hasClass('single-avada_portfolio') ) {
-		if ( !jQuery('body').hasClass('modal-open') || lugar_name != '' ) {
+		if ( !jQuery('body').hasClass('modal-open') || lugar_index != 0 ) {
 			jQuery('#cargando-btn').trigger('click');
 			setTimeout(function(){
 				document.location = doc_href+'?lugar='+lugar_name;
