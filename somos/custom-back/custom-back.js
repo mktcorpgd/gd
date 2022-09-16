708a044699d5c39f6f382b2d150ba20dd08deb2d
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-09-16 10:02:30 -0300

    disabled','disabled

--- somos/custom-back/custom-back.js
+++ somos/custom-back/custom-back.js
@@ -30,7 +30,7 @@ jQuery(document).ready(function(){
 
 
 	// USUARIOS - Perfil: deshabilitar ciertos campos y mover otros
-	jQuery('body.profile-php.no-customize-support input[name=gdCUIL_mc],body.profile-php.no-customize-support input[name=gdPHONE_mc], body.profile-php.no-customize-support input[name=gdUNIT_mc], body.profile-php.no-customize-support input[name=gdANNIV_mc], body.profile-php.no-customize-support input[name=ocp_job_title], body.profile-php.no-customize-support select[name=gdGroupfe587259d3], body.profile-php.no-customize-support select[name=gdGroupa08454c0b0]').attr('disabled','disabled');
+	//jQuery('body.profile-php.no-customize-support input[name=gdCUIL_mc],body.profile-php.no-customize-support input[name=gdPHONE_mc], body.profile-php.no-customize-support input[name=gdUNIT_mc], body.profile-php.no-customize-support input[name=gdANNIV_mc], body.profile-php.no-customize-support input[name=ocp_job_title], body.profile-php.no-customize-support select[name=gdGroupfe587259d3], body.profile-php.no-customize-support select[name=gdGroupa08454c0b0]').attr('disabled','disabled');
 	jQuery('.user-job-title-wrap').insertBefore('.user-unit-wrap');
 	console.log(jQuery('#your-profile tr#password').length);
 	console.log(jQuery('#your-profile .form-table.profile-info').length);
