9c288731c6a759cf3e0a292309a3c178f74e57dd
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-10-05 17:13:21 -0300

    jQuery(e.target)

--- www/js/custom.js
+++ www/js/custom.js
@@ -246,7 +246,7 @@ jQuery(document).ready(function() {
 		});
 		jQuery('html').click(function(e) {
 			console.log(jQuery(e.target));
-			if( !jQuery(e.target).hasClass('expanded') && !jQuery(e.target) != 'div.fusion-portfolio-content-wrapper' ) {
+			if( !jQuery(e.target).hasClass('expanded') && jQuery(e.target) != 'div.fusion-portfolio-content-wrapper' ) {
 				jQuery('.box-buttons article').removeClass('visible').removeClass('expanded').removeClass('opacity');
 			}
 		});
