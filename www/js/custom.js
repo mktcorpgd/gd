51fe0bdccafda567013e9ebe4f3bcc3ed0b3be9d
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-10-05 15:24:52 -0300

    expanded

--- www/js/custom.js
+++ www/js/custom.js
@@ -245,7 +245,7 @@ jQuery(document).ready(function() {
 			e.stopPropagation();
 		});
 		jQuery('html').click(function(e) {
-			if( !jQuery(e.target).hasClass('expanded') ) {
+			if( !jQuery(e.target).hasClass('expanded') || !jQuery(e.target).closest('expanded') ) {
 				jQuery('.box-buttons article').removeClass('visible').removeClass('expanded').removeClass('opacity');
 			}
 		});
