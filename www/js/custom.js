85c9b2611d87f54c283b5636e33595b74a0b1857
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-10-05 21:00:46 -0300

    more-info

--- www/js/custom.js
+++ www/js/custom.js
@@ -622,7 +622,7 @@ jQuery(document).ready(function() {
 				classes_button = 'fusion-button button-flat fusion-button-round button-large button-default button-left"';
 				modal = ' data-toggle="modal" data-target=".fusion-modal.contacto-rapido"';
 			}
-			jQuery('.fusion-portfolio-post:eq('+i+') .fusion-portfolio-content').append('<div class="more-info">'+btn_info+'</div><div class="fusion-buttons"><a class="'+classes_button+'" href="'+link_post+'"><span class="fusion-button-text">'+modal_btn_left_lang+'</span></a><a class="fusion-button button-flat fusion-button-round button-large button-default button-right fusion-modal-text-link" '+modal+'><span class="fusion-button-text">'+modal_btn_right_lang+'</span></a></div>');
+			jQuery('.fusion-portfolio-post:eq('+i+') .fusion-portfolio-content').append('<a href="#info" class="more-info">'+btn_info+'</a><div class="fusion-buttons"><a class="'+classes_button+'" href="'+link_post+'"><span class="fusion-button-text">'+modal_btn_left_lang+'</span></a><a class="fusion-button button-flat fusion-button-round button-large button-default button-right fusion-modal-text-link" '+modal+'><span class="fusion-button-text">'+modal_btn_right_lang+'</span></a></div>');
 		});
 	}
 
