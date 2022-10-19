9acf67c4a497625589f92e47d2abcc10192c1ff0
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2022-10-19 14:34:28 -0300

    textarea[name=ADDRESS

--- somos/custom-front/custom-front-tarjetas.js
+++ somos/custom-front/custom-front-tarjetas.js
@@ -164,7 +164,7 @@ jQuery(document).ready(function() {
 				arr_address[1] = '';
 			}
 			var address = arr_address.join(';');
-			jQuery('input[name=ADDRESS]').val(address);
+			jQuery('textarea[name=ADDRESS]').val(address);
 			jQuery('.tpdata.address').html(address.replace(/\n/g, '<br />'));
 		}
 		if ( span_class == 'phone' || span_class == 'phone_ext' ) {
