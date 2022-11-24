f8f7fe5dfd8a390741da027ea538bb284f5be9d3
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2022-11-24 15:14:40 -0300

    DATEGO

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -394,7 +394,7 @@ jQuery(document).ready(function() {
 					inputtext_name = inputtext_name.substr(0,5);
 					jQuery('input[name='+inputtext_name+'FROM]').val('—').attr('readonly',true);
 					jQuery('input[name='+inputtext_name+'TO]').val('—').attr('readonly',true);
-					jQuery('input[name^='+inputtext_name+'DATETO]').val('').attr('disabled',true);
+					jQuery('input[name^='+inputtext_name+'DATEGO]').val('').attr('disabled',true);
 					jQuery('input[name^='+inputtext_name+'DATEBACK]').val('').attr('disabled',true);
 				}
 				else if ( checkbox_name.indexOf('NOT') > -1 ) {
