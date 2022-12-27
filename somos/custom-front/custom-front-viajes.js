d4f7c336b4acf72049a454e733dce93140ba4048
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-12-27 15:41:57 -0300

    name=RESP

--- somos/custom-front/custom-front-viajes.js
+++ somos/custom-front/custom-front-viajes.js
@@ -138,6 +138,14 @@ jQuery(document).ready(function() {
 	// Asignar responsable según oficina
 	var gdOFFICE_mc = jQuery('input[name=gdOFFICE_mc]').val();
 	gdOFFICE_mc = gdOFFICE_mc.substring(0,2);
-	console.log(gdOFFICE_mc);
+	if ( gdOFFICE_mc == 'AR' ) {
+		jQuery('input[name=RESP]').val(jQuery('input[name=RESPAR]').val());
+	}
+	else if ( gdOFFICE_mc == 'CL' ) {
+		jQuery('input[name=RESP]').val(jQuery('input[name=RESPCL]').val());
+	}
+	else {
+		jQuery('input[name=RESP]').val(jQuery('input[name=RESPAR]').val());
+	}
 
 });
