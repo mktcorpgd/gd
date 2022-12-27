3f1e76583049d34494ee33397bf727cef79e4ce8
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2022-12-27 15:35:27 -0300

    gdORG_mc

--- somos/custom-front/custom-front-viajes.js
+++ somos/custom-front/custom-front-viajes.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function() {
 	jQuery('input[name=PHONE]').val(jQuery('input[name=gdPHONE_mc]').val());
 	function samedataPaxChecked() {
 		jQuery('input[name="SAMEDATAPAX[]"]').trigger('click');
-		jQuery('select[name=ORG] option:contains('+jQuery('input[name=gdGroupa08454c0b0]').val()+')').prop('selected','selected');
+		jQuery('select[name=ORG] option:contains('+jQuery('input[name=gdORG_mc]').val()+')').prop('selected','selected');
 		jQuery('select[name=CC_UNIT] option:contains('+jQuery('input[name=gdUNIT_mc]').val()+')').prop('selected','selected');
 		jQuery('input[name=FNAMEPAX1]').val(jQuery('input[name=FNAME]').val());
 		jQuery('input[name=LNAMEPAX1]').val(jQuery('input[name=LNAME]').val());
@@ -134,5 +134,8 @@ jQuery(document).ready(function() {
 		}
 	});
 
-	
+
+	// Asignar responsable según oficina
+	jQuery('input[name=gdGroupfe587259d3]')
+
 });
