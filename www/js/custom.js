bdc152c0347b750f9dcc497ac7293127f980d8fb
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2023-01-23 17:02:40 -0300

    .html()

--- www/js/custom.js
+++ www/js/custom.js
@@ -129,7 +129,7 @@ jQuery(document).ready(function() {
 
 
 	// CONTENIDOS - Si slider está vacío:
-	jQuery('#sliders-container').trim();
+	jQuery('#sliders-container').html().trim();
 	console.log(jQuery('#sliders-container').is(':empty'));
 	if ( jQuery('#sliders-container').is(':empty') ) {
 		jQuery('#sliders-container').addClasS('empty');
