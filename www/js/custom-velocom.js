0f9ff0c34204327b8cc9ae6de0923341084da67a
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2023-03-31 15:02:27 -0300

    normalize

--- www/js/custom-velocom.js
+++ www/js/custom-velocom.js
@@ -1,3 +1,7 @@
+// GENERAL - Función para normalizar textos
+var normalize=(function(){var from="ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",to="AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",mapping={};for(var i=0,j=from.length;i<j;i++)mapping[from.charAt(i)]=to.charAt(i);return function(str){var ret=[];for(var i=0,j=str.length;i<j;i++){var c=str.charAt(i);if(mapping.hasOwnProperty(str.charAt(i))){ret.push( mapping[c]);}else{ret.push(c);}}return ret.join('').replace(/[^-A-Za-z0-9]+/g,'-').toLowerCase();}})();
+
+
 // HEADER - Transformar teléfonos en link:
 jQuery('.fusion-contact-info-phone-number').each(function(i) {
 	var value = jQuery(this).text();
