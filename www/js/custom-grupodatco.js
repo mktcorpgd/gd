16fbe320d67fe6fef1fbbe75920b2f2474f2f7fe
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2023-05-31 16:57:01 -0300

    ctry_url

--- www/js/custom-grupodatco.js
+++ www/js/custom-grupodatco.js
@@ -79,5 +79,43 @@ jQuery(document).ready(function() {
 			jQuery(this).find('a').attr('href',curhref+'?ctry='+ctry);
 		});
 	}
+	if ( window.location.href.indexOf('?ctry') > -1 ) {
+		var urlParams = new URLSearchParams(window.location.search);
+		ctry_url = urlParams.get('ctry');
+		switch(ctry) {
+			case 'ar':
+				var ctry_name = 'Argentina';
+				var ctry_url = 'ar';
+				break;
+			case 'br':
+				var ctry_name = 'Brasil';
+				var ctry_url = 'br';
+				break;
+			case 'cl':
+				var ctry_name = 'Chile';
+				var ctry_url = 'cl';
+				break;
+			case 'mx':
+				var ctry_name = 'México';
+				var ctry_url = 'mx';
+				break;
+			case 'pe':
+				var ctry_name = 'Perú';
+				var ctry_url = 'pe';
+				break;
+			case 'pr':
+				var ctry_name = 'Puerto Rico';
+				var ctry_url = 'pr';
+				break;
+			case 'uy':
+				var ctry_name = 'Uruguay';
+				var ctry_url = 'uy';
+				break;
+		}
+		jQuery('.fusion-logo-link').attr('href','/'+ctry_url);
+		jQuery('.country-flag.menu-item').removeClass('hidden');
+		jQuery('.country-flag.menu-item > a > span.menu-text').empty();
+		jQuery('.country-flag.menu-item > a > span.menu-text').html('<img src="/wp-content/uploads/flag-'+ctry_url+'.svg" alt="'+ctry_name+'" /><span> '+ctry_name+'</span>');
+	}
 
 });
