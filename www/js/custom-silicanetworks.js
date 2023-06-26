06a85fdf8fc6fe236ab8c7f992cf9a7bd67d7a09
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2023-06-26 16:41:10 -0300

    fullInfo

--- www/js/custom-silicanetworks.js
+++ www/js/custom-silicanetworks.js
@@ -5,34 +5,6 @@ jQuery(document).ready(function() {
 		jQuery('#wpcf7-f31529-p32849-o1 .wpcf7-submit').val('Descargar mapa de red');
 	}
 
-	// Copiar información en campo oculto
-	function fullInfo() {
-		var msg = jQuery('textarea[name=MSG]').val();
-		if ( jQuery('body').hasClass('page-id-36729') || jQuery('body').hasClass('parent-pageid-36729') ) {
-			var srv = jQuery('select[name=SRV] option:selected').val();
-			var srv_index = jQuery('select[name=SRV]').prop('selectedIndex');
-			if ( srv_index == 1 || srv_index == 2 ) {
-				var srvspeed = jQuery('select[name=SRVSPEED] option:selected').val();
-				srv = srv+' ('+srvspeed+')';
-			}		
-			if ( msg.length > 0 ) {
-				jQuery('input[name=PAINSF]').val(srv+' / '+msg);
-			}
-			else {
-				jQuery('input[name=PAINSF]').val(srv);
-			}
-		}
-		else {
-			jQuery('input[name=PAINSF]').val(msg);
-		}
-	}
-	jQuery('select[name=SRV],select[name=SRVSPEED').change(function() {
-		fullInfo();
-	});
-	jQuery('textarea[name=MSG]').on('input',function() {
-		fullInfo();
-	});
-
 	// Cambio URL en logo
 	if ( jQuery('.type-srv').length ) {
 		var type_srv = jQuery('.type-srv a').attr('href');
--- www/js/custom.js
+++ www/js/custom.js
@@ -328,7 +328,34 @@ jQuery(document).ready(function() {
 	}
 
 
-	// FORMULARIOS - Salesforce: asignar "Web"
+	// FORMULARIOS - Salesforce
+	function fullInfo() {
+		var msg = jQuery('textarea[name=MSG]').val();
+		if ( jQuery('body').hasClass('page-id-36729') || jQuery('body').hasClass('parent-pageid-36729') ) {
+			var srv = jQuery('select[name=SRV] option:selected').val();
+			var srv_index = jQuery('select[name=SRV]').prop('selectedIndex');
+			if ( srv_index == 1 || srv_index == 2 ) {
+				var srvspeed = jQuery('select[name=SRVSPEED] option:selected').val();
+				srv = srv+' ('+srvspeed+')';
+			}		
+			if ( msg.length > 0 ) {
+				jQuery('input[name=PAINSF]').val(srv+' / '+msg);
+			}
+			else {
+				jQuery('input[name=PAINSF]').val(srv);
+			}
+		}
+		else {
+			jQuery('input[name=PAINSF]').val(msg);
+		}
+	}
+	jQuery('select[name=SRV],select[name=SRVSPEED').change(function() {
+		fullInfo();
+	});
+	jQuery('textarea[name=MSG]').on('input',function() {
+		fullInfo();
+	});
+
 	if ( jQuery('input[name="ORIGSF"]').length ) {
 		jQuery('input[name="ORIGSF"]').val('Web');
 	}
