4f90a790a3fbc2b6702d8cde901144031e1114cc
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2023-09-28 12:27:42 -0300

    tribe-events-calendar

--- www/js/custom.js
+++ www/js/custom.js
@@ -61,7 +61,7 @@ jQuery(document).ready(function() {
   
 
 	// CONTENIDOS - Abrir en una nueva pestaña
-	jQuery(document).on('click','#prensa .fusion-post-content.post-content>h2>a,#prensa span.meta-tags a',function(e) {
+	jQuery(document).on('click','#prensa .fusion-post-content.post-content>h2>a,#prensa span.meta-tags a,.tribe-events-calendar-list__event:not(.cat_propios)',function(e) {
 		window.open(this.href,'_blank');
 		e.preventDefault();
 		e.stopPropagation();
