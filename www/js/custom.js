18aff1187e5b5d76807c9a8783d7a31e42c4033b
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2023-10-03 12:58:54 -0300

    fusion-fullwidth.country-flag

--- www/js/custom.js
+++ www/js/custom.js
@@ -156,55 +156,6 @@ jQuery(document).ready(function() {
 		});
 	}
 
-	// Si es una página de país:
-	if ( jQuery('.fusion-fullwidth.country-flag').length ) {
-		var flag = window.location.pathname;
-		flag = flag.substring(1,flag.length-1);
-		switch(flag) {
-			case 'ar':
-				var ctry_name = 'Argentina';
-				var ctry_url = 'ar';
-				break;
-			case 'br':
-				var ctry_name = 'Brasil';
-				var ctry_url = 'br';
-				break;
-			case 'cl':
-				var ctry_name = 'Chile';
-				var ctry_url = 'cl';
-				break;
-			case 'mx':
-				var ctry_name = 'México';
-				var ctry_url = 'mx';
-				break;
-			case 'pe':
-				var ctry_name = 'Perú';
-				var ctry_url = 'pe';
-				break;
-			case 'pr':
-				var ctry_name = 'Puerto Rico';
-				var ctry_url = 'pr';
-				break;
-			case 'uy':
-				var ctry_name = 'Uruguay';
-				var ctry_url = 'uy';
-				break;
-		}
-		jQuery('.fusion-logo-link,.country-flag.menu-item>a').attr('href','/'+ctry_url);
-		jQuery('.country-flag.menu-item.fusion-dropdown-menu>a>span>img').attr('src','/wp-content/uploads/flag-'+ctry_url+'.svg');
-		jQuery('.country-flag.menu-item.fusion-dropdown-menu').attr('title',ctry_name);
-		jQuery('.country-flag.menu-item > a > span.menu-text').empty().html('<img src="/wp-content/uploads/flag-'+ctry_url+'.svg" alt="'+ctry_name+'" /><span> '+ctry_name+'</span>');
-		jQuery('.fusion-button,.menu-item a').each(function(i) {
-			var curhref = jQuery(this).attr('href');
-			if ( curhref.indexOf('?') > -1 ) {
-				jQuery(this).attr('href',curhref+'&ctry='+ctry_url);
-			}
-			else {
-				jQuery(this).attr('href',curhref+'?ctry='+ctry_url);
-			}
-		});
-	}
-
 
 	// PORTFOLIO - Desktop / Mostrar/ocultar información
 	if ( jQuery(window).width() > 920 ) {
