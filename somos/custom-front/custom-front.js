23ac29150c7297bf75b07acb0e7af95c459bab43
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2023-10-26 15:55:32 -0300

    office.indexOf

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -125,16 +125,13 @@ jQuery(document).ready(function() {
 			}
 		}
 		if ( office.indexOf('AR') > -1 ) {
-			jQuery('.kit .menu-item.cl,.kit .menu-item.mx,.kit .menu-item.pe,.kit .menu-item.pr').hide();
+			jQuery('.kit .menu-item.cl-pe,.kit .menu-item.mx-pr').hide();
 		}
-		if ( office.indexOf('CL') > -1 ) {
-			jQuery('.kit .menu-item.ar,.kit .menu-item.mx,.kit .menu-item.pe,.kit .menu-item.pr').hide();
+		if ( office.indexOf('CL') > -1 || office.indexOf('PE') > -1 ) {
+			jQuery('.kit .menu-item.ar,.kit .menu-item.mx-pr').hide();
 		}
-		if ( office.indexOf('PE') > -1 ) {
-			jQuery('.kit .menu-item.ar,.kit .menu-item.cl,.kit .menu-item.mx,.kit .menu-item.pr').hide();
-		}
-		if ( office.indexOf('MX') > -1 ) {
-			jQuery('.kit .menu-item.ar,.kit .menu-item.cl,.kit .menu-item.mx,.kit .menu-item.pr').hide();
+		if ( office.indexOf('MX') > -1 || office.indexOf('PR') > -1 ) {
+			jQuery('.kit .menu-item.ar,.kit .menu-item.cl-pe').hide();
 		}
 	}
 
