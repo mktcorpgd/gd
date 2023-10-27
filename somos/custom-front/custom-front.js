1260225d52d4bec671e1db35bac0053d58902b37
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2023-10-27 11:12:30 -0300

    :contains("IoP")

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -119,9 +119,9 @@ jQuery(document).ready(function() {
 			jQuery('.kit .menu-item:not(.visible):not(:contains("'+org+'"))').hide();
 			jQuery('.kit .menu-item a[href*="#'+org_class+'"]+.sub-menu>li,.kit .menu-item:contains("Grupo Datco"),.kit .menu-item a[href="#grupo-datco"]+.sub-menu>li').show();
 		}
-		jQuery('.kit .menu-item a:contains("IoP")').hide();
+		jQuery('.kit .menu-item:contains("IoP")').hide();
 		if ( unit.indexOf('SCAI') == -1 ) {
-			jQuery('.kit .menu-item a:contains("IoP")').show();
+			jQuery('.kit .menu-item:contains("IoP")').show();
 		}
 		if ( office.indexOf('AR') > -1 ) {
 			jQuery('.kit .menu-item.cl-pe:not(.ar),.kit .menu-item.mx-pr:not(.ar)').hide();
