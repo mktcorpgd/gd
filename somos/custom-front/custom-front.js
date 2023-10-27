983660b29e39e23bbf2801f02c96a7999bc9c624
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2023-10-27 11:16:54 -0300

    unit.indexOf('SCAI')

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -119,6 +119,7 @@ jQuery(document).ready(function() {
 			jQuery('.kit .menu-item:not(.visible):not(:contains("'+org+'"))').hide();
 			jQuery('.kit .menu-item a[href*="#'+org_class+'"]+.sub-menu>li,.kit .menu-item:contains("Grupo Datco"),.kit .menu-item a[href="#grupo-datco"]+.sub-menu>li').show();
 			jQuery('.kit .menu-item:contains("IoP")').hide();
+			console.log(unit.indexOf('SCAI'));
 			if ( unit.indexOf('SCAI') == -1 ) {
 				jQuery('.kit .menu-item:contains("IoP")').show();
 			}
