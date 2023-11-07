930f77c55a7fc6aad361cedd7f58d01f73555e14
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2023-11-07 16:18:32 -0300

    hide

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -126,7 +126,7 @@ jQuery(document).ready(function() {
 		}
 		if ( unit.indexOf('UMAC') > -1 || unit.indexOf('UN') > -1 || unit.indexOf('PM') > -1 ) {
 			jQuery('.kit .menu-item:not(:contains("'+org+'")):not("Grupo Datco")').hide();
-			jQuery('.kit .menu-item:contains("'+org+'"),.kit .menu-item:contains("'+org+'")>ul>li,.kit .menu-item a[href="#grupo-datco"]+.sub-menu>li').show();
+			jQuery('.kit .menu-item a[href="#grupo-datco"]+.sub-menu>li').show();
 		}
 		if ( unit.indexOf('UAC') > -1 || unit.indexOf('UMAC') > -1 || unit.indexOf('UN') > -1 || unit.indexOf('PM') > -1 ) {
 			jQuery('.kit .menu-item.pt').show();
