9b519953863b02ae2c116d418eab00bcb68f29f6
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2024-02-21 17:36:50 -0300

    prop('disabled

--- www/js/custom.js
+++ www/js/custom.js
@@ -525,7 +525,7 @@ jQuery(document).ready(function() {
 
 	// FORMULARIOS - Animación para etiquetas
 	jQuery(':input[type=text],:input[type=email],:input[type=number],:input[type=tel],textarea,select').each(function(i){
-		if ( !jQuery(this).prop('disabled') ) {
+		if ( !jQuery(this).prop('disabled') == true ) {
 			if ( jQuery(this).val().length > 0 ) {
 				jQuery(this).parents('.fusion-layout-column').addClass('focused');
 			}
