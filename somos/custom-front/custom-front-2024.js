0d98f445c8d0d2833f127da6f67bbada17e1c26c
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2024-03-25 16:24:31 -0300

    nav_menu

--- somos/custom-front/custom-front-2024.js
+++ somos/custom-front/custom-front-2024.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function() {
 
 	jQuery('#logo img').attr('src','https://somos.grupodatco.com/wp-content/uploads/logo-somosgd.svg');
+	jQuery('#nav_menu-2').insertBefore('#ajaxsearchpro1_1');
 	
 });
