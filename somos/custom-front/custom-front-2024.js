542eb71d4a0fda0e2e5a4bcbda62fda4feaaefc6
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2024-03-26 15:26:50 -0300

    .widget .widgettitle

--- somos/custom-front/custom-front-2024.js
+++ somos/custom-front/custom-front-2024.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function() {
 
 	// GENERAL - Si hay cumpleaños:
 	if ( jQuery('.bdays_today').length ) {
+		jQuery('.bdays_today img').attr('src','/wp-content/uploads/gd2024-birth.jpg');
 	}
 
 });
