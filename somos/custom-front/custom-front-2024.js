575d92c17926b6180cfb10260061ddd62395350c
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2024-03-27 12:41:22 -0300

    #content-wrapper

--- somos/custom-front/custom-front-2024.js
+++ somos/custom-front/custom-front-2024.js
@@ -5,4 +5,10 @@ jQuery(document).ready(function() {
 		jQuery('.bdays_today img').attr('src','/wp-content/uploads/gd2024-birth.jpg');
 	}
 
+	// Desplegables
+	jQuery('#custom_html-11 .toggle-box').show();
+	jQuery('#custom_html-11 .toggle .fa-plus-square').removeClass('fa-plus-square').addClass('fa-minus-square');
+	jQuery('#custom_html-24 .toggle-box').show();
+	jQuery('#custom_html-24 .toggle .fa-plus-square').removeClass('fa-plus-square').addClass('fa-minus-square');
+
 });
