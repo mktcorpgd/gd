983a5b2ab99de2cbcbb39e36d8732f09532191a2
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2024-03-27 15:54:22 -0300

    cumple-1.png

--- somos/custom-front/custom-front-2024.js
+++ somos/custom-front/custom-front-2024.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
 
 	// GENERAL - Si hay cumpleaños:
 	if ( jQuery('.bdays_today').length ) {
-		jQuery('.bdays_today img').attr('src','/wp-content/uploads/cumple.png');
+		jQuery('.bdays_today img').attr('src','/wp-content/uploads/cumple-1.png');
 	}
 
 	// Desplegables
