8511ab614408bfd25d4277218bef3e580f04e75b
MKTCORP GD <98123392+nalonsogd@users.noreply.github.com>
2024-04-22 11:12:14 -0300

    id_form+' .tp').removeClass().addClass('tp '+tp_org

--- somos/custom-front/custom-front-tarjetas.js
+++ somos/custom-front/custom-front-tarjetas.js
@@ -82,11 +82,12 @@ jQuery(document).ready(function() {
 			tp_org = tp_org.replace(/ /,'_').toLowerCase();
 			tp_org = tp_org.replace(/\(|\)/g,'');
 			console.log(tp_org);
+			jQuery('#'+id_form+' .tp').removeClass().addClass('tp '+tp_org);
 			if ( tp_org.indexOf('0g') != -1 ) {
 				tp_org = tp_org = tp_org.replace(/0g/g,'zerog');
 			}
 			if ( id_form == 'wpcf7-f24224-p18546-o2' ) {
-				jQuery('#'+id_form+' .tp').removeClass().addClass('tp digital '+tp_org);
+				jQuery('#'+id_form+' .tp').addClass('digital');
 				jQuery('#mobile-logo').fadeOut('fast', function() {
 				jQuery('#mobile-logo').attr('src','/wp-content/uploads/'+tp_org+'-ima_h-blanco.png');
 				}).fadeIn('fast');
