a977a6cb3039edd5351c9d70c568410aa1f9f0f2
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2024-09-20 17:28:15 -0300

    console.log

--- somos/custom-front/custom-front.js
+++ somos/custom-front/custom-front.js
@@ -453,8 +453,8 @@ jQuery(document).ready(function() {
 		id_form = parseInt(id_form.substring(id_form.indexOf('wpcf7-f')+7,id_form.indexOf('-p')));
 		jQuery('.wpcf7-select option').each(function(i) {
 			// General - Marcar como deshabilitado si tiene — delante (<optgroup> no disponible en CF7)
-			console.log(jQuery(this).text() != '—');
-			if ( jQuery(this).text().indexOf('—') > -1 && (jQuery(this).text() != '—' || jQuery(this).text() != '&#xFEFF;—') ) {
+			console.log(jQuery(this).text() == '—');
+			if ( jQuery(this).text().indexOf('—') > -1 && jQuery(this).text() != '—' ) {
 				jQuery(this).attr('disabled','disabled');
 			}
 			// Casos de referencia - Si es formulario para enviar nuevo caso
