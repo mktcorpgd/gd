d5cc2de5bffbfc7031f78ca930e54a43045d1844
Nicolás Alonso <nicolas.alonso@datco.net>
2024-10-23 11:03:39 -0300

    lugar_name

--- www/js/custom-velocom.js
+++ www/js/custom-velocom.js
@@ -62,15 +62,6 @@ jQuery('select[name=LOC]').change(function() {
 			jQuery('div:not(.not-selected) .'+lugar_class+'+.not').hide();
 			jQuery('.plan:not(.'+lugar_class+')').hide();
 			jQuery('select[name="LOC"]').val(lugar_name);
-			jQuery('select[name="LOC"]').each(function() {
-				jQuery(this).find('option').each(function() {
-					if (jQuery(this).val() == lugar_name) {
-						jQuery(this).attr('selected', 'selected');
-					}
-					jQuery(this).removeAttr('selected');
-				});
-				jQuery$(this).trigger('change');
-			});
 			jQuery('select[name="LOC"]').parents('.fusion-layout-column').addClass('focused');
 			jQuery('div:not(.not-selected) .'+lugar_class+',.tfs-slider span.'+lugar_class+',.caption span.'+lugar_class+',.legales .panel-body span.'+lugar_class+',.legales .cur_month,.legales .cur_year,.legales .last_day,.step2').show();
 			var lugar_index = jQuery('option:selected',this).index();
