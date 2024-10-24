96bd2eb01ec4d2f859a3d36ab5906c13306f98be
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2024-10-24 15:06:11 -0300

    val(lugar_name).trigger

--- www/js/custom-velocom.js
+++ www/js/custom-velocom.js
@@ -64,7 +64,7 @@ jQuery('select[name="LOC"]').off('change').change(function() {
 			jQuery('.caption,.price span,.legales,.not-selected').show();
 			jQuery('div:not(.not-selected) .'+lugar_class+'+.not').hide();
 			jQuery('.plan:not(.'+lugar_class+')').hide();
-			jQuery('select[name="LOC"]').val(lugar_name);
+			jQuery('select[name="LOC"]').val(lugar_name).trigger('blur');
 			jQuery('label[for="LOC"]').parents('.fusion-layout-column').addClass('focused');
 			jQuery('div:not(.not-selected) .'+lugar_class+',.tfs-slider span.'+lugar_class+',.caption span.'+lugar_class+',.legales .panel-body span.'+lugar_class+',.legales .cur_month,.legales .cur_year,.legales .last_day,.step2').show();
 			if ( jQuery('.map iframe').length ) {
