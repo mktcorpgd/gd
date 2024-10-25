e09c68023cf11c46b9a12e43a5a557fd8fbf4983
Nicolás Alonso <alonsonicolasalejandro@gmail.com>
2024-10-25 09:25:53 -0300

    jQuery('label[for="LOC"]').parent().addClass('focused');

--- www/js/custom-velocom.js
+++ www/js/custom-velocom.js
@@ -31,7 +31,7 @@ if ( jQuery('body').hasClass('home') || jQuery('body').hasClass('single-avada_po
 		jQuery('div:not(.not-selected) .'+lugar_class+'+.not').hide();
 		jQuery('.plan:not(.'+lugar_class+')').hide();
 		jQuery('select[name="LOC"] option.'+lugar_class+',select[name="LOC"] option:contains('+lugar_name+')').attr('selected','selected');
-		jQuery('select[name="LOC"]').parents('.fusion-layout-column').addClass('focused');
+		jQuery('label[for="LOC"]').parent().addClass('focused');
 		jQuery('div:not(.not-selected) .'+lugar_class+',.tfs-slider span.'+lugar_class+',.caption span.'+lugar_class+',.legales .panel-body span.'+lugar_class+',.legales .cur_month,.legales .cur_year,.legales .last_day,.step2').show();
 		var lugar_index = jQuery('option:selected',this).index();
 		if ( jQuery('.map iframe').length ) {
@@ -65,7 +65,7 @@ jQuery('select[name="LOC"]').on('change', function() {
 				jQuery('div:not(.not-selected) .'+lugar_class+'+.not').hide();
 				jQuery('.plan:not(.'+lugar_class+')').hide();
 				jQuery('select[name="LOC"]').val(lugar_name).trigger('change');
-				jQuery('select[name="LOC"]').parents('.fusion-layout-column').addClass('focused');
+				jQuery('label[for="LOC"]').parent().addClass('focused');
 				jQuery('div:not(.not-selected) .'+lugar_class+',.tfs-slider span.'+lugar_class+',.caption span.'+lugar_class+',.legales .panel-body span.'+lugar_class+',.legales .cur_month,.legales .cur_year,.legales .last_day,.step2').show();
 				if ( jQuery('.map iframe').length ) {
 					jQuery('.map iframe').attr('src','https://www.velocom.com.ar/_velocom/cobertura/'+lugar_class);
