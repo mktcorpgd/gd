43545b71458caa427cc8c65a0691d1a7937f7fd5
Nicolás Alonso <nicolas.alonso@datco.net>
2024-11-26 10:31:41 -0300

    address

--- somos/custom-front/custom-front-viajes.js
+++ somos/custom-front/custom-front-viajes.js
@@ -76,8 +76,8 @@ jQuery(document).ready(function($) {
 				var address_text = this.text;
 				console.log(address);
 				console.log(address_text);
-				var img_src = 'https://maps.googleapis.com/maps/api/staticmap?size=600x150&maptype=roadmap&markers=size:mid%7Ccolor:red%7C'+address_text+'&zoom=14&key=AIzaSyBgKiaX5D3Pp4Jx16S_JBH4_vUJngmX3PM';
-				var html_map = '<a href="https://www.google.com/maps/search/'+address_text+'" target="_blank"><img src="'+img_src+'" /></a>';
+				var img_src = 'https://maps.googleapis.com/maps/api/staticmap?size=600x150&maptype=roadmap&markers=size:mid%7Ccolor:red%7C'+address+'&zoom=14&key=AIzaSyBgKiaX5D3Pp4Jx16S_JBH4_vUJngmX3PM';
+				var html_map = '<a href="https://www.google.com/maps/search/'+address+'" target="_blank"><img src="'+img_src+'" /></a>';
 				$('input[name='+input_name+'].static_gmap').addClass('filled');
 				if ( $('input[name='+input_name+'].static_gmap+a').length ) {
 					$('input[name='+input_name+'].static_gmap+a').replaceWith(html_map);	
