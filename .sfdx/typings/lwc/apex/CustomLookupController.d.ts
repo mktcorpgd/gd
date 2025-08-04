0d28e5400d34dceca0aa3b5071ebf586eb0c6e62
Nicolás Alonso <nicolas.alonso@datco.net>
2025-08-04 10:40:18 -0300

    sfdx

--- .sfdx/typings/lwc/apex/CustomLookupController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/CustomLookupController.searchRecords" {
-  export default function searchRecords(param: {searchKey: any, objectName: any}): Promise<any>;
-}
-declare module "@salesforce/apex/CustomLookupController.updateRecordField" {
-  export default function updateRecordField(param: {parentId: any, parentObject: any, fieldApi: any, value: any}): Promise<any>;
-}
